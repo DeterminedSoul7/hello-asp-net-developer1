d346eb45ef60802cb95e9ab436edb23919ebc332
alumnos <u202411402@upc.edu.pe>
2026-04-08 10:20:24 -0500

    feat(greeting): add GreetDeveloper request and response.

---
+++ Generic/Interfaces/REST/Resources/GreetDeveloperResponse.cs
@@ -0,0 +1,16 @@
+namespace DefaultNamespace;
+
+/// <summary>
+/// A record representing the response for a greeting request.
+/// </summary>
+/// <param name="Id"></param>
+/// <param name="FirstName"></param>
+/// <param name="Message"></param>
+public record GreetDeveloperResponse(Guid? Id, string? FirstName, string Message)
+{
+    /// <summary>
+    /// Constructs a response with only a message, used for anonymous greetings.
+    /// </summary>
+    /// <param name="message">The greeting message.</param>
+    public GreetDeveloperResponse(string message) : this(null, null, message) { }
+}
